ef5995c8b6e992f278f399327d123029087575e0
rique223 <henrique.jobs1@gmail.com>
2022-03-09 16:27:01 -0300

    Debbugging procfile

--- src/main/kotlin/br/com/devfs/projetodevfskotlin/GoogleMapsModel.kt
+++ src/main/kotlin/br/com/devfs/projetodevfskotlin/GoogleMapsModel.kt
@@ -1,6 +1,7 @@
 package br.com.devfs.projetodevfskotlin
 
 import config.MapsProps
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.ResponseEntity
