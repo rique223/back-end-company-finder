4b9e4554614a96e3d3bda5a26a1568defc2927f5
rique223 <henrique.jobs1@gmail.com>
2022-03-09 19:42:41 -0300

    Debbuging response type

--- src/main/kotlin/br/com/devfs/projetodevfskotlin/GeocodedAddress.kt
+++ src/main/kotlin/br/com/devfs/projetodevfskotlin/GeocodedAddress.kt
@@ -1,62 +1,69 @@
 package br.com.devfs.projetodevfskotlin
 
 data class GeocodedAddress(
-    val results: List<Result>,
-    val status: String
+    val results: List<Result>?,
+    val status: String?
 )
 
 data class Result(
-    val address_components: List<AddressComponent>,
-    val formatted_address: String,
-    val geometry: Geometry,
-    val place_id: String,
-    val types: List<String>
+    val address_components: List<AddressComponent>?,
+    val formatted_address: String?,
+    val geometry: Geometry?,
+    val partial_match: Boolean?,
+    val place_id: String?,
+    val plus_code: PlusCode?,
+    val types: List<String>?
 )
 
 data class AddressComponent(
-    val long_name: String,
-    val short_name: String,
-    val types: List<String>
+    val long_name: String?,
+    val short_name: String?,
+    val types: List<String>?
 )
 
 data class Geometry(
-    val bounds: Bounds,
-    val location: Location,
-    val location_type: String,
-    val viewport: Viewport
+    val bounds: Bounds?,
+    val location: Location?,
+    val location_type: String?,
+    val viewport: Viewport?
+)
+
+data class PlusCode(
+    val compound_code: String?,
+    val global_code: String?
 )
 
 data class Bounds(
-    val northeast: Northeast,
-    val southwest: Southwest
+    val northeast: Northeast?,
+    val southwest: Southwest?
 )
 
 data class Location(
-    val lat: Double,
-    val lng: Double
+    val lat: Double?,
+    val lng: Double?
 )
 
 data class Viewport(
-    val northeast: NortheastX,
-    val southwest: SouthwestX
+    val northeast: NortheastX?,
+    val southwest: SouthwestX?
 )
 
 data class Northeast(
-    val lat: Double,
-    val lng: Double
+    val lat: Double?,
+    val lng: Double?
 )
 
 data class Southwest(
-    val lat: Double,
-    val lng: Double
+    val lat: Double?,
+    val lng: Double?
 )
 
 data class NortheastX(
-    val lat: Double,
-    val lng: Double
+    val lat: Double?,
+    val lng: Double?
 )
 
 data class SouthwestX(
-    val lat: Double,
-    val lng: Double
+    val lat: Double?,
+    val lng: Double?
 )
